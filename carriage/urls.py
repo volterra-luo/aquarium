9eeb7462341934fe7bb2c464d530b683b5a781e2
volterra <volterra.luo@gmail.com>
2013-09-10 00:04:29 +0800

    testing 3-rd party login auth

--- carriage/urls.py
+++ carriage/urls.py
@@ -7,4 +7,6 @@ urlpatterns = patterns('',
     url(r'^$', views.IndexView.as_view()),
     url(r'statistics/$',views.view_stat, name='stat'),
     url(r'^(?P<crg_id>[^/]+)/$', views.view_page,name='index'),
+    url(r'^accounts/google/login/$', views.google_login, name='social_google_login'),
+    url(r'^accounts/google/login/done/$', views.google_auth, name='social_google_login_done'),
 )
--- carriage/views.py
+++ carriage/views.py
@@ -40,3 +40,72 @@ def view_page(request,crg_id):
 
 def view_stat(request):
     return render_to_response('carriage/stat.html',)
+
+def _get_referer_url(request):
+    referer_url = request.META.get('HTTP_REFERER', '/')
+    host = request.META['HTTP_HOST']
+    if referer_url.startswith('http') and host not in referer_url:
+        referer_url = '/'
+    return referer_url
+
+import urllib
+def google_login(request):
+    google_auth_url = '%s?%s' % ('https://accounts.google.com/o/oauth2/auth',
+                             urllib.urlencode({
+                                 'response_type': 'code',
+                                 'client_id': '554792660883-l7mvvejqvkev3qaa23bmlv13ivfg2mil.apps.googleusercontent.com',
+                                 'redirect_uri': 'http://localhost:8000/carriage/',
+                                 'scope': 'https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile',
+                                 'state': _get_referer_url(request)
+                             }))
+    return HttpResponseRedirect(google_auth_url)
+
+import urllib2
+def get_access_token(code):
+    auth_url = 'https://accounts.google.com/o/oauth2/token'
+    body = urllib.urlencode({
+                'code': code, # 授权码
+                'client_id': '554792660883-l7mvvejqvkev3qaa23bmlv13ivfg2mil.apps.googleusercontent.com',
+                'client_secret': 'kROd_3tyYvXHxjHiXwUD0EtP',
+                'redirect_uri': 'http://localhost:8000/carriage/',
+                'grant_type': 'authorization_code' # 必须是这个值
+                })
+    headers = {
+        'Content-Type': 'application/x-www-form-urlencoded',
+    }
+    req = urllib2.Request(auth_url, body, headers)
+    resp = urllib2.urlopen(req)
+     
+    data = json.loads(resp.read())
+     
+    return data['access_token']
+
+def get_user_info(access_token):
+    if access_token:
+        userinfo_url = 'https://www.googleapis.com/oauth2/v1/userinfo'
+        query_string = urllib.urlencode({'access_token': access_token})
+         
+        resp = urllib2.urlopen("%s?%s" % (userinfo_url, query_string))
+        data = json.loads(resp.read())
+         
+        return data
+    
+def google_auth(request):
+    if 'blog_user' in request.session:
+        return HttpResponseRedirect('/')
+     
+    if 'error' in request.GET or 'code' not in request.GET:
+        return HttpResponseRedirect('/')
+     
+    code = request.GET['code']
+     
+    access_token = get_access_token(code)
+    blog_user = get_blog_user(get_user_info(access_token))
+     
+    request.session['blog_user'] = blog_user
+     
+    next = '/'
+    if 'state' in request.GET:
+        next = request.GET['state']
+     
+    return HttpResponseRedirect(next)
